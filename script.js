fe9fb452dad31bdf1ead8f5867fb48af053d8e97
jazmand <atothezmand@gmail.com>
2020-11-04 22:59:54 +0900

    updated

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 $('.flip-container .flipper').click(function() {
-	$(this).closest('.flip-container').toggleClass('hover');
+	$(this).closest('.flip-container').toggleClass('click');
     $(this).css('transform, rotateY(180deg)');
+});
+
+AOS.init({
+    easing: 'ease',
+    duration: 1800
 });
