f401b46465bdb4845f058093d75c6b30719e446c
jazmand <atothezmand@gmail.com>
2020-11-07 00:13:20 +0900

    updated

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-$('.flip-container .flipper').click(function() {
-	$(this).closest('.flip-container').toggleClass('click');
-    $(this).css('transform, rotateY(180deg)');
-});
+$(document).ready(function() {
+    $('.flip-container .flipper').click(function() {
+        $(this).closest('.flip-container').toggleClass('click');
+        $(this).css('transform, rotateY(180deg)');
+    });
 
-AOS.init({
-    easing: 'ease',
-    duration: 1800
-});
+    // $('#arrow-up').on('click', function() {
+    //     $('html, body').animate(keyframes: {
+    //         scrollTop: 0
+    //     }, options: 2000);
+    // });
+
+    AOS.init({
+        easing: 'ease',
+        duration: 1800
+    });
+});    
