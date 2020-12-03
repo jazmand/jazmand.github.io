40aad89d249c2f788a919bc46d09f7dcd66e2b5c
jazmand <atothezmand@gmail.com>
2020-12-03 18:42:58 +0900

    updated

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
         $(this).closest('.flip-container').toggleClass('click');
         $(this).css('transform, rotateY(180deg)');
     });
-
-    AOS.init({
-        easing: 'ease',
-        duration: 1800
-    });
 });    
+
+AOS.init({
+    easing: 'ease',
+    duration: 1800
+});
