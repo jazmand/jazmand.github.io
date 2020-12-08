600d76826bfbdabc351c706b572b4f061161cd33
jazmand <atothezmand@gmail.com>
2020-12-09 00:21:09 +0900

    updated

--- script.js
+++ script.js
@@ -10,20 +10,3 @@ AOS.init({
     duration: 1800
 });
 
-var cube = document.querySelector('.cube');
-var radioGroup = document.querySelector('.radio-group');
-var currentClass = '';
-
-function changeSide() {
-  var checkedRadio = radioGroup.querySelector(':checked');
-  var showClass = 'show-' + checkedRadio.value;
-  if ( currentClass ) {
-    cube.classList.remove( currentClass );
-  }
-  cube.classList.add( showClass );
-  currentClass = showClass;
-}
-// set initial side
-changeSide();
-
-radioGroup.addEventListener( 'change', changeSide );
