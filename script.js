54db8935b0604b0ee4fdcce852c4fea4d28e6adf
jazmand <atothezmand@gmail.com>
2020-12-08 17:58:55 +0900

    cube inserted

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ AOS.init({
     easing: 'ease',
     duration: 1800
 });
+
+var cube = document.querySelector('.cube');
+var radioGroup = document.querySelector('.radio-group');
+var currentClass = '';
+
+function changeSide() {
+  var checkedRadio = radioGroup.querySelector(':checked');
+  var showClass = 'show-' + checkedRadio.value;
+  if ( currentClass ) {
+    cube.classList.remove( currentClass );
+  }
+  cube.classList.add( showClass );
+  currentClass = showClass;
+}
+// set initial side
+changeSide();
+
+radioGroup.addEventListener( 'change', changeSide );
