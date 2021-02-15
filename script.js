70f86f7de8bf9ff843e1b16383ec912113015164
jazmand <atothezmand@gmail.com>
2021-02-15 18:18:06 +0900

    updated animations

--- script.js
+++ script.js
@@ -3,8 +3,14 @@ $('.flip-container .flipper').click(function() {
     $(this).css('transform, rotateY(180deg)');
 });
 
+$('#arrow-up').click(function() {
+    $("html, body").animate({scrollTop: 0}, 1000);
+});
+
 AOS.init({
     easing: 'ease',
     duration: 1800
 });
+
+
 
