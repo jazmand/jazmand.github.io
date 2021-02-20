6004f2222f0d892e42a2f0a1dd446f960be4846b
jazmand <atothezmand@gmail.com>
2021-02-21 02:52:48 +0900

    updated before new nav

--- vancouver-landing-page/app.js
+++ vancouver-landing-page/app.js
@@ -1,12 +1,17 @@
 const menuToggle = document.querySelector('.toggle')
+const logo = document.querySelector('.logo')
 const showcase = document.querySelector('.showcase')
 const icon = document.querySelector('.fa-bars')
-
+const aboutMenu = document.querySelector('.aboutMenu')
 
 menuToggle.addEventListener('click', () => {
     showcase.classList.toggle('active')
     icon.classList.toggle('fa-hamburger')
     menuToggle.classList.toggle('toggleBlack')
+});
+
+logo.addEventListener('click', () => {
+    window.scrollTo(0, 0);
 });
 
 
