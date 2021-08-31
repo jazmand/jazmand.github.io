71f85518e6eae0d3225b8861d0af507101e364bd
jazmand <atothezmand@gmail.com>
2021-08-31 22:17:30 +0900

    remove window on load

--- script.js
+++ script.js
@@ -73,17 +73,16 @@ $(document).ready(function () {
 			that.tick();
 		}, delta);
 	};
-	window.onload = function () {
-		var elements = document.getElementsByClassName('txt-rotate');
-		for (var i = 0; i < elements.length; i++) {
-			var toRotate = elements[i].getAttribute('data-rotate');
-			var period = elements[i].getAttribute('data-period');
-			if (toRotate) {
-				new TxtRotate(elements[i], JSON.parse(toRotate), period);
-			}
+
+	var elements = document.getElementsByClassName('txt-rotate');
+	for (var i = 0; i < elements.length; i++) {
+		var toRotate = elements[i].getAttribute('data-rotate');
+		var period = elements[i].getAttribute('data-period');
+		if (toRotate) {
+			new TxtRotate(elements[i], JSON.parse(toRotate), period);
 		}
-		var css = document.createElement('style');
-		css.innerHTML = '.txt-rotate > .wrap { border-right: 0.08em solid #666 }';
-		document.body.appendChild(css);
-	};
+	}
+	var css = document.createElement('style');
+	css.innerHTML = '.txt-rotate > .wrap { border-right: 0.08em solid #666 }';
+	document.body.appendChild(css);
 });
